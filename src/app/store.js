f81c74fd95cbaf5b8497cbf27556b1274c784727
Chris Turner <ChristopherMTurner@outlook.com>
2019-02-26 12:25:14 -0700

    Finished up the app

--- src/app/store.js
+++ src/app/store.js
@@ -15,5 +15,43 @@ export const store = {
     submitEvent(eventDetails) {
         const activeDay = this.getActiveDay();
         activeDay.events.push({ "details": eventDetails, "edit": false });
+    },
+    editEvent(dayId, eventDetails) {
+        this.resetEditOfAllEvents();
+        const eventObj = this.getEventObj(dayId, eventDetails);
+        eventObj.edit = true;
+    },
+    updateEvent(dayId, originalEventDetails, newEventDetails) {
+        const eventObj = this.getEventObj(dayId, originalEventDetails);
+        // Set the event details to the new details
+        // and turn off editing
+        eventObj.details = newEventDetails;
+        eventObj.edit = false;
+    },
+    deleteEvent(dayId, eventDetails) {
+        const dayObj = this.state.seedData.find(
+            day => day.id === dayId
+        );
+        const eventIndexToRemove = dayObj.events.findIndex(
+            event => event.details === eventDetails
+        );
+        dayObj.events.splice(eventIndexToRemove, 1);
+    },
+    getEventObj(dayId, eventDetails) {
+        // Find the day object
+        const dayObj = this.state.seedData.find(
+            day => day.id === dayId
+        );
+        // Return the specific event
+        return dayObj.events.find(
+            event => event.details === eventDetails
+        );
+    },
+    resetEditOfAllEvents() {
+        this.state.seedData.map((dayObj) => {
+            dayObj.events.map((event) => {
+                event.edit = false;
+            });
+        });
     }
 }
